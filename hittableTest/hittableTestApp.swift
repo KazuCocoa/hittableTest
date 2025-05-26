668eb4905ba052df82bbf125862845cbe27dbd7b
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-05-25 18:52:16 -0700

    Initial Commit

---
+++ hittableTest/hittableTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  hittableTestApp.swift
+//  hittableTest
+//
+//  Created by Kazuaki Matsuo on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct hittableTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hittableTestTests/hittableTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  hittableTestTests.swift
+//  hittableTestTests
+//
+//  Created by Kazuaki Matsuo on 5/25/25.
+//
+
+import Testing
+@testable import hittableTest
+
+struct hittableTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
